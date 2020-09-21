f93733436358adad92118c5ac90c09c02263a72a
sucan <719659178@qq.com>
2020-09-21 22:33:57 +0800

    补题

--- atcoder/src/abc179/E.java
+++ atcoder/src/abc179/E.java
@@ -7,14 +7,34 @@ import java.util.Scanner;
  */
 public class E {
     private static int MOD = 998244353;
+    private static int[] vis = new int[100005];
+    private static long[] sum = new long[100005];
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
         long x = scanner.nextLong();
-        long m = scanner.nextLong();
-        for(int i = 0;i<50;i++){
-            System.out.println(x);
-            x = (x*x)%m;
+        long mod = scanner.nextLong();
+        long ans = 0;
+        int index = 1;
+        int startIndex = -1;
+        for(;index<=n;index++){
+            if(vis[(int)x] != 0){
+                startIndex = vis[(int)x];
+                break;
+            }
+            vis[(int)x] = index;
+            sum[index] = sum[index-1]+x;
+            ans += x;
+            x = (x*x)%mod;
         }
+        if(n >= index){
+            n = n - index + 1;
+            ans = ans + (n/(index-startIndex))*(sum[index-1]-sum[startIndex-1]);
+            if(n%(index-startIndex) != 0){
+                int xx = (int)(n%(index-startIndex));
+                ans = ans + sum[startIndex+xx-1] -sum[startIndex-1];
+            }
+        }
+        System.out.println(ans);
     }
 }
