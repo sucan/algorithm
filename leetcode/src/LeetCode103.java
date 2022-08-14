a0ba09a678edfdac6a53ca6bfdfb3aac64cbff31
sucan <719659178@qq.com>
2022-08-14 17:09:22 +0800

    leetcode

---
+++ leetcode/src/LeetCode103.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @Auther: kep
+ * @Date: 2022/8/14 15:50
+ * @Description:
+ */
+public class LeetCode103 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Node {
+        public Node(int level, TreeNode node){
+            this.level = level;
+            this.treeNode = node;
+        }
+        int level = 0;
+        TreeNode treeNode;
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0,root));
+        LinkedList<Integer> tmp = new LinkedList<>();
+        int flag = 0;
+        int level = 0;
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            if(node.treeNode == null){
+                continue;
+            }
+            if(node.level != level){
+                ans.add(tmp);
+                tmp = new LinkedList<>();
+                level = node.level;
+                flag++;
+                flag %= 2;
+            }
+            if(flag % 2 == 0) {
+                tmp.addLast(node.treeNode.val);
+            }else{
+                tmp.addFirst(node.treeNode.val);
+            }
+            if(node.treeNode.left != null){
+                queue.add(new Node(node.level +1,node.treeNode.left));
+            }
+            if(node.treeNode.right != null){
+                queue.add(new Node(node.level +1,node.treeNode.right));
+            }
+        }
+        if(tmp.size() > 0){
+            ans.add(tmp);
+        }
+        return ans;
+    }
+}
---
+++ leetcode/src/LeetCode106.java
@@ -0,0 +1,52 @@
+/**
+ * @Auther: kep
+ * @Date: 2022/8/14 16:21
+ * @Description:
+ */
+public class LeetCode106 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return buildTree(0,inorder.length-1,0,postorder.length -1,inorder,postorder);
+    }
+
+    private TreeNode buildTree(int il,int ir,int pl,int pr,int[] inorder, int[] postorder){
+        if(il > ir || pl > pr || il < 0 || pl < 0){
+            return null;
+        }
+        if(il == ir){
+            return new TreeNode(inorder[il]);
+        }
+        if(pl == pr){
+            return new TreeNode(postorder[pl]);
+        }
+        TreeNode node = new TreeNode(postorder[pr]);
+        int imid = 0;
+        for(int i = il;i<=ir;i++){
+            if(inorder[i] == postorder[pl]){
+                imid = i;
+                break;
+            }
+        }
+        node.right = buildTree(imid+1,ir,pr-(ir-imid),pr-1,inorder,postorder);
+        node.left = buildTree(il,imid-1,pl,pr-(ir-imid)-1,inorder,postorder);
+        return node;
+    }
+}
