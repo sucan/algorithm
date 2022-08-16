3798b394bc3e61a72fc57b3a0d8cf47ac232eae2
sucan <719659178@qq.com>
2022-08-16 23:33:17 +0800

    leetcode

---
+++ leetcode/src/LeetCode114.java
@@ -0,0 +1,44 @@
+/**
+ * @Auther: kep
+ * @Date: 2022/8/16 22:16
+ * @Description:
+ */
+public class LeetCode114 {
+    private TreeNode currentIndex;
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public void flatten(TreeNode root) {
+        currentIndex = root;
+        convertTree2List(root);
+    }
+
+    private void convertTree2List(TreeNode current){
+        if(current == null){
+            return;
+        }
+        currentIndex = current;
+        TreeNode tmp = current.right;
+        current.right = current.left;
+        current.left = null;
+        convertTree2List(current.right);
+        currentIndex.right = tmp;
+        convertTree2List(tmp);
+    }
+}
---
+++ leetcode/src/LeetCode116.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Auther: kep
+ * @Date: 2022/8/16 22:50
+ * @Description:
+ */
+public class LeetCode116 {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public Node connect(Node root) {
+        List<Node> currentNode = new ArrayList<>();
+        currentNode.add(root);
+        while(!currentNode.isEmpty()){
+            List<Node> tmp = new ArrayList<>();
+            Node pre = null;
+            for(Node node:currentNode){
+                if(node == null){
+                    continue;
+                }
+                if(pre != null){
+                    pre.next = node;
+                }
+                pre= node;
+                if(node.left != null) {
+                    tmp.add(node.left);
+                }
+                if(node.right != null) {
+                    tmp.add(node.right);
+                }
+            }
+            currentNode = tmp;
+        }
+        return root;
+    }
+}
