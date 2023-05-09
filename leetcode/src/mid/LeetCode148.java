bda3c1118403ccb70185dcfea9582e2630e92c6d
sucan <719659178@qq.com>
2023-05-09 23:31:09 +0800

    2 mid

--- leetcode/src/mid/LeetCode148.java
+++ leetcode/src/mid/LeetCode148.java
@@ -1,14 +1,14 @@
 package mid;
 
 /**
- * 归并排序或者快排
+ * 归并排序或者堆排序
  * @Auther: kep
  * @Date: 2023/4/23 23:42
  * @Description:
  */
 public class LeetCode148 {
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
@@ -26,6 +26,93 @@ public class LeetCode148 {
     }
 
     public ListNode sortList(ListNode head) {
+        int length = getLength(head);
+        int step = 1;
+        ListNode emptyHead = new ListNode();
+        emptyHead.next = head;
+        for(int i = step;i<length;i*=2){
+            ListNode currentNode = emptyHead.next;
+            ListNode preHead = emptyHead;
+            ListNode s1Head = null;
+            ListNode s2Head = null;
+            while(currentNode != null) {
+                s1Head = currentNode;
+                for(int j = 1;j<i && currentNode.next != null;j++){
+                    currentNode = currentNode.next;
+                }
+                s2Head = currentNode.next;
+                currentNode.next = null;
+                currentNode = s2Head;
+                for(int j = 1;j<i && currentNode != null;j++){
+                    currentNode = currentNode.next;
 
+                }
+                if(currentNode != null){
+                    ListNode next = currentNode.next;
+                    currentNode.next = null;
+                    currentNode = next;
+                }
+                preHead.next = mergeList(s1Head, s2Head);
+                    while(preHead.next != null){
+                        preHead = preHead.next;
+                    }
+
+
+
+            }
+        }
+        return emptyHead.next;
+    }
+
+    private ListNode mergeList(ListNode s1,ListNode s2){
+        ListNode emptyNode = new ListNode();
+        ListNode tmp = emptyNode;
+        ListNode tmp1 = s1;
+        ListNode tmp2 = s2;
+        while(tmp1 != null || tmp2 != null){
+            if(tmp1 == null){
+                tmp.next = tmp2;
+                tmp2= tmp2.next;
+            }else if(tmp2 == null){
+                tmp.next = tmp1;
+                tmp1= tmp1.next;
+            }else if(tmp1.val < tmp2.val){
+                tmp.next = tmp1;
+                tmp1= tmp1.next;
+            }else{
+                tmp.next = tmp2;
+                tmp2= tmp2.next;
+            }
+            tmp = tmp.next;
+        }
+        return emptyNode.next;
+    }
+
+    private int getLength(ListNode head){
+        int x = 0;
+        ListNode tmp = head;
+        while(tmp !=null){
+            tmp = tmp.next;
+            x++;
+        }
+        return x;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode();
+        head.val = -1;
+        ListNode next = new ListNode();
+        next.val = 5;
+        head.next = next;
+        ListNode next2 = new ListNode();
+        next2.val = 3;
+        next.next = next2;
+        ListNode next3 = new ListNode();
+        next3.val = 4;
+        next2.next = next3;
+        ListNode next4 = new ListNode();
+        next4.val = 0;
+        next3.next = next4;
+        new LeetCode148().sortList(head);
     }
 }
---
+++ leetcode/src/mid/LeetCode149.java
@@ -0,0 +1,65 @@
+package mid;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Auther: kep
+ * @Date: 2023/5/5 22:25
+ * @Description:
+ */
+public class LeetCode149 {
+    public int maxPoints(int[][] points) {
+        int max = 0;
+        for(int i = 0;i<points.length;i++){
+            Map<Long,Integer> map = new HashMap<>();
+            for(int j = i+1;j<points.length;j++){
+                Long slope  = getSlope(points[i],points[j]);
+                Integer nums = map.get(slope);
+                if(map.get(slope) == null){
+                    nums = 1;
+                }
+                nums++;
+                map.put(slope,nums);
+                max = Math.max(max,nums);
+            }
+        }
+        return max;
+    }
+
+    private long getSlope(int[] point1,int[] point2){
+        int y  = (point2[1]-point1[1]);
+        int x = (point2[0]-point1[0]);
+        if(y == 0){
+            return 200001;
+        }
+        if(x == 0){
+            return 200002;
+        }
+        int gcd = getGcd(Math.abs(y),Math.abs(x));
+        long result = Math.abs(y/gcd * 100000L) + Math.abs(x/gcd);
+        if(sameSign(x,y)){
+            return result;
+        }
+        return -result;
+    }
+
+    private int getGcd(int a,int b){
+        if(b == 0){
+            return a;
+        }
+        return getGcd(b,a%b);
+    }
+
+    boolean sameSign(int a, int b) {
+        if ( a >= 0 )
+            return b >= 0;
+        else
+            return b < 0;
+    }
+
+    public static void main(String[] args) {
+        int[][] input = {{2,3},{3,3},{-5,3}};
+        new LeetCode149().maxPoints(input);
+    }
+}
