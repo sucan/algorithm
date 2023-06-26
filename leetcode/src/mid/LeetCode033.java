3ce7d009d8c51290ecba02cc5de1aef4fb2e74b8
sucan <719659178@qq.com>
2023-06-27 00:25:52 +0800

    1mid

--- leetcode/src/mid/LeetCode033.java
+++ leetcode/src/mid/LeetCode033.java
@@ -9,6 +9,58 @@ package mid;
  */
 public class LeetCode033 {
 
+
+    public int search(int[] nums, int target) {
+        int l = 0,r = nums.length-1;
+        while(l <=r ){
+            int mid = (l+r)>>1;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[l] == nums[r]){
+                return -1;
+            }
+            if(nums[l] < nums[r]){
+                if(nums[mid] > target){
+                    r = mid -1;
+                }else{
+                    l = mid + 1;
+                }
+            }else{
+                if(nums[mid] < nums[r]){
+                    if(nums[mid] > target){
+                        r = mid -1;
+                    }else{
+                        if(nums[r] ==target){
+                            return r;
+                        }
+                        if(nums[r] > target){
+                            l = mid + 1;
+                        }else{
+                            r = mid -1;
+                        }
+                    }
+                }else if(nums[mid] == nums[r]){
+                    r = mid - 1;
+                }else{
+                    if(nums[mid] < target){
+                        l = mid + 1;
+                    }else{
+                        if(nums[l] == target){
+                            return l;
+                        }
+                        if(nums[l] < target){
+                            r = mid -1;
+                        }else{
+                            l = mid +1;
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
     public int search_classic(int[] nums, int target) {
         int l = 0,r = nums.length-1;
         while(l <= r){
